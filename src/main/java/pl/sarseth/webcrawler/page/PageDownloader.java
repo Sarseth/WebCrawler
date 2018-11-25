bfda58a6fcf31cf5eed46878d59497597b3f84c6
Sarseth <elune90@gmail.com>
2018-11-25 18:48:40 +0100

    WC_02: Page downloader

---
+++ src/main/java/pl/sarseth/webcrawler/page/PageDownloader.java
@@ -0,0 +1,26 @@
+package pl.sarseth.webcrawler.page;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+import java.net.UnknownHostException;
+import java.util.Optional;
+
+public class PageDownloader {
+
+    private static final String USER_AGENT = "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:63.0) Gecko/20100101 Firefox/63.0";
+
+    public Optional<Document> downloadPage(String url) {
+        try {
+            var document = Jsoup.connect(url).userAgent(USER_AGENT).get();
+            return Optional.of(document);
+        } catch (UnknownHostException e) {
+            System.out.println(url + " does not exist.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return Optional.empty();
+    }
+
+}
---
+++ src/test/java/pl/sarseth/webcrawler/page/PageDownloaderTest.java
@@ -0,0 +1,45 @@
+package pl.sarseth.webcrawler.page;
+
+
+import org.jsoup.nodes.Document;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+@DisplayName("Confirmation that pages are downloaded properly")
+class PageDownloaderTest {
+
+    private PageDownloader pageDownloader = new PageDownloader();
+
+    @Test
+    @DisplayName("Simple check if it's possible to download webpage")
+    void checkIfPageIsDownloadedCorrectly() {
+        // GIVEN
+        String url = "https://google.pl";
+
+        // WHEN
+        // WHEN
+        Optional<Document> document = pageDownloader.downloadPage(url);
+
+        // THEN
+        assertThat(document.isEmpty(), is(false));
+    }
+
+    @Test
+    @DisplayName("Non existing pages should not throw java.net.UnknownHostException")
+    void nonExistingHostTest() {
+        // GIVEN
+        String url = "http://www.this.page.does.not.exists.for.sure.or.is.it.gov/";
+
+        // WHEN
+        Optional<Document> document = pageDownloader.downloadPage(url);
+
+        // THEN
+        assertThat(document.isEmpty(), is(true));
+    }
+
+}
